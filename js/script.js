229c30dd7760cb04f5dd2a6ee477f256ba90ef9b
anggitmu <anggit.muttaqin2497@gmail.com>
2025-11-07 23:52:16 +0700

    form submit added

--- js/script.js
+++ js/script.js
@@ -13,3 +13,22 @@ function welcomeMessage() {
 function formValidation() {
 
 }
+
+function display() {
+    let name = document.forms["inquiriesForm"]["name"].value;
+    let email = document.forms["inquiriesForm"]["email"].value;
+    let choice = document.forms["inquiriesForm"]["radio"].value;
+    let date = document.forms["inquiriesForm"]["date"].value;
+    let message = document.forms["inquiriesForm"]["message"].value;
+    document.getElementById("namespan").textContent = 'Hello '+name+', welcome to Wanefwan';
+    document.getElementById("datespan").textContent = date;
+    document.getElementById("messagespan").textContent = name;
+    document.getElementById("emailspan").textContent = email;
+    document.getElementById("aside").classList.remove("invisible");
+    // console.log('Hello '+name+', welcome to Wanefwan');
+    // document.getElementById("disp").innerHTML = "name";
+//   if (x == "") {
+//     alert("Name must be filled out");
+//     return false;
+//   }
+}
